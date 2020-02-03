b444a92eaf7de302cd4e1e3477f29778f17498d6
ChristianPorras <chrisporras1@uchicago.edu>
2020-02-03 06:52:18 +0000

    finished omnicalc tutorial

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,111 @@
 class ApplicationController < ActionController::Base
+  def flex_square
+    # The Parameters go into "params"
+    the_num = params.fetch("num").to_i
+    the_square = the_num**2
 
+    # output_message = "The square of #{the_num} is #{the_square}"
+
+    output_hash = {
+      :number => the_num,
+      :square => the_square
+    }
+
+    render({:plain => output_hash.to_json})
+  end 
+
+  def flex_sqrt
+    # The Parameters go into "params"
+    the_num = params.fetch("num").to_i
+    the_sqrt = the_num**(0.5)
+
+    # output_message = "The square of #{the_num} is #{the_square}"
+
+    output_hash = {
+      :number => the_num,
+      :square_root => the_sqrt
+    }
+
+    render({:plain => output_hash.to_json})
+  end 
+
+  def rand_range
+    # The Parameters go into "params"
+    min = params.fetch("min").to_i
+    max = params.fetch("max").to_i
+
+    rand_num = rand(min...max)
+
+    # output_message = "The square of #{the_num} is #{the_square}"
+
+    output_hash = {
+      :min => min,
+      :max => max,
+      :random => rand_num
+    }
+
+    render({:plain => output_hash.to_json})
+  end 
+
+  def query_square
+    number = params.fetch("input_number").to_f
+    square = number**2
+
+    output_hash = {
+      :number => number.to_i,
+      :square => square.to_i
+    }
+
+    render({:plain => output_hash.to_json})
+  end
+
+  def query_sqrt
+    number = params.fetch("input_number").to_f
+    sqrt = number**(0.5)
+
+    output_hash = {
+      :number => number.to_i,
+      :square_root => sqrt
+    }
+
+    render({:plain => output_hash.to_json})
+  end
+
+  def query_range
+    min = params.fetch("input_min").to_f
+    max = params.fetch("input_max").to_f
+
+    rando = rand(min...max)
+
+    output_hash = {
+      :min => min,
+      :max => max,
+      :random => rando
+    }
+
+    render({:plain => output_hash.to_json})
+  end
+
+  def monthly_payment
+    apr = params.fetch("input_apr").to_f
+    years = params.fetch("input_years").to_i
+    price = params.fetch("input_pv").to_f
+
+    r = (apr/12)/100
+    mos = 12 * years
+
+    numer = (r)*price
+    denom = (1 - (1+(r))**(-1*mos))
+
+    payment = (numer/denom).round(2)
+
+    output_hash = {
+      :purchase_price => price.to_i,
+      :apr => apr,
+      :years => years,
+      :monthly_payment => payment
+    }
+
+    render({:plain => output_hash.to_json})
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,19 @@
 Rails.application.routes.draw do
 
+  get("/flexible/square/:num", {:controller => "application", :action => "flex_square"})
+
+  get("/flexible/square_root/:num", {:controller => "application", :action => "flex_sqrt"})
+
+  get("/flexible/random/:min/:max", {:controller => "application", :action => "rand_range"})
+
+  get("/square/results", {:controller => "application", :action => "query_square"})
+
+  get("/square_root/results", {:controller => "application", :action => "query_sqrt"})
+
+  get("/random/results", {:controller => "application", :action => "query_range"})
+
+  get("/payment/results", {:controller => "application", :action => "monthly_payment"})
+
   # ================================================================================
   # Write your routes above.
   # ================================================================================
